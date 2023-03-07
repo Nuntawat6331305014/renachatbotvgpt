3191b11624d7f3dbc4aadf73bdff4ef104dc3a8a
Nuntawat6331305014 <6331305014@lamduan.mfu.ac.th>
2023-03-07 20:01:22 +0700

    add api

---
+++ api/index.js
@@ -0,0 +1,81 @@
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const operandClient = require("@operandinc/sdk").operandClient;
+const indexIDHeaderKey = require("@operandinc/sdk").indexIDHeaderKey;
+const ObjectService = require("@operandinc/sdk").ObjectService;
+require('dotenv').config();
+
+// Open AI Configuration
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// Express Configuration
+const app = express();
+const port = 3080;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(require("morgan")("dev"));
+
+// Routing
+
+// Primary Open AI Route
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log(message)
+  const runIndex = async () => {
+    const operand = operandClient(
+      ObjectService,
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      "https://api.operand.ai",
+      {
+        [indexIDHeaderKey]: "xxxxxxxxxxxxxxxxxxxx",
+      }
+    );
+
+    try {
+      const results = await operand.searchWithin({
+        query: `${message}`,
+        limit: 5,
+      });
+
+      if (results) {
+        return results.matches.map((m) => `- ${m.content}`).join("\n");
+      } else {
+        return "";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  let operandSearch = await runIndex(message);
+
+  const basePromptPrefix = `This is a conversation between ATAmanager and a stranger.\nRelevant information that ATAmanager knows:\n${operandSearch}`;
+  console.log(basePromptPrefix);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}\n\nStranger:${message}\n\nATAmanager:`,
+    max_tokens: 256,
+    temperature: 0.7,
+  });
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+// Get Models Route
+
+// Start the server
+app.listen(port, () => {
+  console.log(`server running`);
+});
+
+module.exports = app;
